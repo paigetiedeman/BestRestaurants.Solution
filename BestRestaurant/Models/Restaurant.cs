4c8e61fd8522ac2727040d8678ed5d26913ad363
Paige Tiedeman <paigetiedeman@gmail.com>
2021-10-06 15:19:49 -0700

    HTML files updated with Price and Rating
    Co-authored-by: Nick Hennessy <njhnny@gmail.com>

--- BestRestaurant/Models/Restaurant.cs
+++ BestRestaurant/Models/Restaurant.cs
@@ -4,7 +4,10 @@ namespace BestRestaurant.Models
   {
     public int RestaurantId { get; set; }
     public string Name { get; set; }
+    public string Price { get; set; }
+    public string Rating { get; set; }
     public int CuisineId { get; set; }
     public virtual Cuisine Cuisine { get; set; }
+    
   }
 }
